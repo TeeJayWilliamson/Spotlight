7238bc7a39424e234f8ea3240f1996b65ee593b8
TeeJayWilliamson <trevorjwilliamson@gmail.com>
2024-12-19 09:38:16 -0500

    port changes again

--- backend/server.js
+++ backend/server.js
@@ -134,11 +134,11 @@ app.post('/send-emblem', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 // Handle all GET requests by sending back the React app's index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
