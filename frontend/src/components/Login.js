5adb4b87fe4a1e0cf030e02cff8bb2420902e17f
TeeJayWilliamson <trevorjwilliamson@gmail.com>
2025-07-28 12:58:46 -0400

    reverted to Jan 2021 and CORs

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -25,7 +25,7 @@ function Login({ setAuth, setUsername }) {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${apiUrl.replace(/\/$/, '')}/login`, {
+    const response = await axios.post(`${apiUrl.replace(/\/$/, '')}/auth/login`, {
         username,
         password,
       });
